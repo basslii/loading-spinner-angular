212a1ba3c323c0b101e66f47bcf907ca0eab0a6c
Bazli <bm89821@win.edag.de>
2022-10-20 11:19:02 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
